1a94c33df72cfe9528d1c2effa90a50edee5433e
utkukaya <utku.sabri1@hotmail.com>
2021-01-20 01:24:58 +0300

    changes

--- server.py
+++ server.py
@@ -67,7 +67,7 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     port = app.config.get("PORT", 5000)
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="localhost", port=port)
     
     
 
